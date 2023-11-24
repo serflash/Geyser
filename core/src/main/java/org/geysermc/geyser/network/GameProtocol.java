e6cc5cfdddffaaf5580618377733d64847d53c53
serflash <44881861+serflash@users.noreply.github.com>
2023-11-24 22:49:01 +0300

    Update GameProtocol.java
    add bedrock supports 
    1.17.x - 1,19.x

--- core/src/main/java/org/geysermc/geyser/network/GameProtocol.java
+++ core/src/main/java/org/geysermc/geyser/network/GameProtocol.java
@@ -62,6 +62,60 @@ public final class GameProtocol {
     private static final PacketCodec DEFAULT_JAVA_CODEC = MinecraftCodec.CODEC;
 
     static {
+        SUPPORTED_BEDROCK_CODECS.add(Bedrock_v440.CODEC.toBuilder()
+            .minecraftVersion("1.17.0/1.17.2")
+            .build());
+        SUPPORTED_BEDROCK_CODECS.add(Bedrock_v448.CODEC.toBuilder()
+            .minecraftVersion("1.17.10/1.17.11")
+            .build());
+        SUPPORTED_BEDROCK_CODECS.add(Bedrock_v465.CODEC.toBuilder()
+            .minecraftVersion("1.17.30/1.17.34")
+            .build());
+        SUPPORTED_BEDROCK_CODECS.add(Bedrock_v471.CODEC.toBuilder()
+            .minecraftVersion("1.17.40/1.17.41")
+            .build());
+        SUPPORTED_BEDROCK_CODECS.add(Bedrock_v475.CODEC.toBuilder()
+            .minecraftVersion("1.18.0/1.18.2")
+            .build());
+        SUPPORTED_BEDROCK_CODECS.add(Bedrock_v486.CODEC.toBuilder()
+            .minecraftVersion("1.18.10/1.18.12")
+            .build());
+        SUPPORTED_BEDROCK_CODECS.add(Bedrock_v503.CODEC.toBuilder()
+            .minecraftVersion("1.18.30/1.18.33")
+            .build());
+        SUPPORTED_BEDROCK_CODECS.add(Bedrock_v527.CODEC.toBuilder()
+            .minecraftVersion("1.19.0")
+            .build());
+        SUPPORTED_BEDROCK_CODECS.add(Bedrock_v534.CODEC.toBuilder()
+            .minecraftVersion("1.19.10")
+            .build());
+        SUPPORTED_BEDROCK_CODECS.add(Bedrock_v544.CODEC.toBuilder()
+            .minecraftVersion("1.19.20")
+            .build());
+        SUPPORTED_BEDROCK_CODECS.add(Bedrock_v545.CODEC.toBuilder()
+            .minecraftVersion("1.19.21/1.19.22")
+            .build());
+        SUPPORTED_BEDROCK_CODECS.add(Bedrock_v554.CODEC.toBuilder()
+            .minecraftVersion("1.19.30/1.19.31")
+            .build());
+        SUPPORTED_BEDROCK_CODECS.add(Bedrock_v557.CODEC.toBuilder()
+            .minecraftVersion("1.19.40/1.19.41")
+            .build());
+        SUPPORTED_BEDROCK_CODECS.add(Bedrock_v560.CODEC.toBuilder()
+            .minecraftVersion("1.19.50/1.19.51")
+            .build());
+        SUPPORTED_BEDROCK_CODECS.add(Bedrock_v567.CODEC.toBuilder()
+            .minecraftVersion("1.19.60/1.19.62")
+            .build());
+        SUPPORTED_BEDROCK_CODECS.add(Bedrock_v568.CODEC.toBuilder()
+            .minecraftVersion("1.19.63")
+            .build());
+        SUPPORTED_BEDROCK_CODECS.add(Bedrock_v575.CODEC.toBuilder()
+            .minecraftVersion("1.19.70/1.19.73")
+            .build());
+        SUPPORTED_BEDROCK_CODECS.add(Bedrock_v582.CODEC.toBuilder()
+            .minecraftVersion("1.19.80/1.19.83")
+            .build());
         SUPPORTED_BEDROCK_CODECS.add(Bedrock_v589.CODEC.toBuilder()
             .minecraftVersion("1.20.0/1.20.1")
             .build());
@@ -94,7 +148,7 @@ public final class GameProtocol {
     /* Bedrock convenience methods to gatekeep features and easily remove the check on version removal */
 
     public static boolean isPre1_20_10(GeyserSession session) {
-        return session.getUpstream().getProtocolVersion() < Bedrock_v594.CODEC.getProtocolVersion();
+        return session.getUpstream().getProtocolVersion() < Bedrock_v440.CODEC.getProtocolVersion();
     }
 
     /**
@@ -102,7 +156,7 @@ public final class GameProtocol {
      * @return true if the session needs an experiment for recipe unlocking
      */
     public static boolean isUsingExperimentalRecipeUnlocking(GeyserSession session) {
-        return session.getUpstream().getProtocolVersion() == Bedrock_v594.CODEC.getProtocolVersion();
+        return session.getUpstream().getProtocolVersion() == Bedrock_v440.CODEC.getProtocolVersion();
     }
 
     /**
